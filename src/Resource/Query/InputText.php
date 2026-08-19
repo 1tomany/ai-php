05b7b2cf2d539a8d8642f69d83ac1db990b28a3b
Vic Cherubini <vcherubini@gmail.com>
2026-08-19 13:51:44 -0500

    wip

--- src/Resource/Query/InputText.php
+++ src/Resource/Query/InputText.php
@@ -5,10 +5,7 @@ namespace OneToMany\AI\Resource\Query;
 use OneToMany\AI\Exception\InvalidArgumentException;
 use OneToMany\AI\Exception\RuntimeException;
 
-use function file_exists;
 use function file_get_contents;
-use function is_file;
-use function is_readable;
 use function sprintf;
 use function trim;
 
@@ -32,39 +29,25 @@ final readonly class InputText implements \Stringable
     }
 
     /**
-     * @throws InvalidArgumentException when the file does not exist
-     * @throws InvalidArgumentException when the path is not a file
-     * @throws InvalidArgumentException when the file is not readable
-     * @throws RuntimeException when reading the file fails
-     * @throws InvalidArgumentException when the file is empty
-     */
-    public static function fromFile(string $path): self
-    {
-        if (!file_exists($path)) {
-            throw new InvalidArgumentException(sprintf('The input text file "%s" does not exist.', $path));
-        }
-
-        if (!is_file($path)) {
-            throw new InvalidArgumentException(sprintf('The input text path "%s" is not a file.', $path));
-        }
-
-        if (!is_readable($path)) {
-            throw new InvalidArgumentException(sprintf('The input text file "%s" is not readable.', $path));
-        }
-
-        if (false === $text = @file_get_contents($path)) {
-            throw new RuntimeException(sprintf('Reading the input text file "%s" failed.', $path));
-        }
-
-        return new self($text);
-    }
-
-    /**
-     * @see \Stringable::__toString()
+     * @see \Stringable
      */
     #[\Override]
     public function __toString(): string
     {
         return $this->text;
+    }
+
+    /**
+     * @see OneToMany\AI\Resource\Query\InputText::__construct()
+     *
+     * @throws RuntimeException when reading the file fails
+     */
+    public static function fromFile(string $path): self
+    {
+        if (false === $text = @file_get_contents($path)) {
+            throw new RuntimeException(sprintf('Reading the input text file "%s" failed.', $path));
+        }
+
+        return new self($text);
     }
 }
--- src/Resource/Query/Usage.php
+++ src/Resource/Query/Usage.php
@@ -6,16 +6,16 @@ final readonly class Usage
 {
     /**
      * @param non-negative-int $inputTokens
-     * @param non-negative-int $outputTokens
      * @param non-negative-int $cachedInputTokens
      * @param non-negative-int $reasoningTokens
+     * @param non-negative-int $outputTokens
      * @param non-negative-int $totalTokens
      */
     public function __construct(
         public int $inputTokens = 0,
-        public int $outputTokens = 0,
         public int $cachedInputTokens = 0,
         public int $reasoningTokens = 0,
+        public int $outputTokens = 0,
         public int $totalTokens = 0,
     ) {
     }
